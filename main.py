049d23f3f40e3bb49665e312a6d1f012c5e6f07b
Deepanshi Gaur <dgaur1@binghamton.edu>
2021-09-23 15:36:12 -0400

    final commit

--- main.py
+++ main.py
@@ -18,7 +18,28 @@ michelangelo.goto(-100,20)
 leonardo.goto(-100,-20)
 
 ## 5. your code goes here
+def race():
+  distance = random.randrange(1,100)
+  leonardo.forward(distance)
+  michelangelo.forward(distance)
+  
+race() 
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 
+from time import sleep
+window.tracer(0)
+
+while True:
+    sleep(0.1)
+    michelangelo.forward(random.randrange(0,10))
+    leonardo.forward(random.randrange(0, 10))
+    window.update()
+
+race()
+
+michelangelo.goto(-100,20)
+leonardo.goto(-100,-20)
 
 # Part B - complete part B here
 
